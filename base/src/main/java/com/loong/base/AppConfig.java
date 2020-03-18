3eba5a0441b350040da79c8a9db6c7df11e945ab
wangyongsheng <wangyongsheng@01zhuanche.com>
2020-03-18 21:37:21 +0800

    update

--- base/src/main/java/com/loong/base/AppConfig.java
+++ base/src/main/java/com/loong/base/AppConfig.java
@@ -1,6 +1,8 @@
 package com.loong.base;
 
 public class AppConfig {
+
+    //需要初始化的组件的application
     private static final String LoginApp = "com.loong.login.LoginApp";
 
     public static String[] moduleApps = {
